a63c85b34cf48a5ccc29ce099a80e7bba9d2cae0
deyaberger <bergerdeya@gmail.com>
2023-03-27 19:34:26 +0200

    avec treatment in function

--- src/data_parsing.py
+++ src/data_parsing.py
@@ -2,14 +2,18 @@
 import argparse
 import matplotlib.pyplot as plt
 from typing import List
+import logging
+import os
 
-from mne import Epochs, pick_types, events_from_annotations, annotations_from_events
+from mne import Epochs, pick_types, events_from_annotations, annotations_from_events, read_epochs
 from mne.channels import make_standard_montage
 from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
 from mne import set_log_level
 set_log_level("WARNING")
 
+logger = logging.getLogger("data_parsing")
+logger.setLevel(level=logging.WARNING)
 
 class Visualizer():
     """Class to Visualize MNE data."""
@@ -26,7 +30,7 @@ class Visualizer():
     @classmethod
     def show_raw_data(self, raw, smooth=False):
         """Plot row data (Volts of signals for each channel along the time)."""
-        print("Plotting raw data\n")
+        logger.info("Plotting raw data\n")
         if smooth:
             raw.plot(scalings=dict(eeg=1e-4), title="Raw Data")
         else:
@@ -49,25 +53,27 @@ class Parser():
     noisy_channels = ['AF7', 'AF3', 'AFz', 'AF4', 'AF8', 'Fp1', 'Fpz', 'Fp2', 'P7', 'P5', 'P3', 'P1', 'P2', 'P4', 'P6',
                             'P8', 'PO7', 'PO3', 'POz', 'PO4', 'PO8', 'Iz']
 
-    def __init__(self, **kwargs):
+    def __init__(self, run_id: int = None, **kwargs):
         """Initialize data path, subject number and run task."""
         self.subject = kwargs['subject']
-        self.runs = kwargs['runs']
+        self.run = kwargs['run']
         self.mne_path = kwargs['mne_path']
+        self.run_id = run_id
         self.events = None
         self.annotations = None
         self.epochs = None
+        self.picks = None
 
     def load_data(self):
         """Load data."""
-        print(f"Loading data for subject {self.subject}, at runs {self.runs}\n")
-        raw_fnames = eegbci.load_data(self.subject, self.runs, path=self.mne_path)
+        logger.info(f"Loading data for subject {self.subject}, at run {self.run}\n")
+        raw_fnames = eegbci.load_data(self.subject, self.run, path=self.mne_path)
         self.raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
         eegbci.standardize(self.raw)
 
     def select_montage(self, montage_name):
         """Select the montage that better fits motor imagery task."""
-        print(f"Selecting montage: {montage_name}\n")
+        logger.info(f"Selecting montage: {montage_name}\n")
         montage = make_standard_montage(montage_name)
         self.raw.set_montage(montage, on_missing='ignore')
         return montage
@@ -83,11 +89,11 @@ class Parser():
                         the electrical activity of nearby power lines
         - noisy_channels = Channels that do are not related to the motion cortex.
         """
-        print("Reducing noise:")
+        logger.info("Reducing noise:")
         if noisy_freq is not None:
-            print(f"Removing frequency {noisy_freq}")
+            logger.info(f"Removing frequency {noisy_freq}")
             self.raw.notch_filter(noisy_freq, method="iir")
-        print(f"Removing bad or noisy channels: {noisy_channels}\n")
+        logger.info(f"Removing bad or noisy channels: {noisy_channels}\n")
         picks = pick_types(self.raw.info, eeg=True,
                            exclude=noisy_channels)
         self.raw.pick(picks)
@@ -95,13 +101,13 @@ class Parser():
 
     def focus_and_clean(self, significant_frequencies):
         """Focus on the frequencies that are relevant for motor imagery."""
-        print("Focussing on significant frequencies\n")
+        logger.info("Focussing on significant frequencies\n")
         start, end = significant_frequencies
         self.raw.filter(start, end)
 
     def get_events(self, event_id=None):
         """Get events."""
-        print("Getting events from annotations\n")
+        logger.info("Getting events from annotations\n")
         if event_id:
             self.events, self.event_id = events_from_annotations(self.raw, event_id=event_id)
         else:
@@ -109,23 +115,55 @@ class Parser():
 
     def get_annotations(self, labels=None):
         """Get annotations."""
-        print("Getting annotations from events\n")
+        logger.info("Getting annotations from events\n")
         if labels:
             self.annotations = annotations_from_events(events=self.events, event_desc=labels, sfreq=self.raw.info["sfreq"])
         else:
             self.annotations = annotations_from_events(events=self.events, sfreq=self.raw.info["sfreq"])
 
-    def get_epochs(self, labels, baseline: float = None, tmin: float = -1., tmax: float = 4.0, montage_name: str = 'standard_1005', noisy_channels: List[str] = noisy_channels, noisy_freq: int = 60):
+    def motion_preprocessing(self, labels: dict,
+                   montage_name: str = 'standard_1005',
+                   noisy_channels: List[str] = noisy_channels, noisy_freq: int = 60):
         self.load_data()
         self.select_montage(montage_name)
+        self.get_events()
+        self.get_annotations(labels)
         self.reduce_noise(noisy_freq, noisy_channels)
-        picks = self.reduce_noise()
+        self.picks = self.reduce_noise()
         self.focus_and_clean(significant_frequencies=(7.0, 32.0))
-        self.get_events(labels)
-        if baseline:
-            self.epochs = Epochs(self.raw, self.events, self.event_id, tmin, tmax, proj=True, picks=picks, baseline=(tmin, tmin + 1.), preload=True)
+        self.get_events()
+
+    def get_epochs_path(self, dir: str, run_id: int, subject_nb: int)-> str:
+        self.epochs_dir = dir
+        self.epochs_run_dir = f"run_{run_id}"
+        self.filename = f"S{subject_nb:03d}_epo.fif"
+        return f"{self.epochs_dir}/{self.epochs_run_dir}/{self.filename}"
+
+    def create_epochs_dir_if_not_exists(self):
+        if not os.path.exists(self.epochs_dir):
+            os.mkdir(self.epochs_dir)
+        if not os.path.exists(f"{self.epochs_dir}/{self.epochs_run_dir}"):
+            os.mkdir(f"{self.epochs_dir}/{self.epochs_run_dir}")
+
+    def get_epochs(self,
+                   baseline: float = None,
+                   tmin: float = -1., tmax: float = 4.0,
+                   epochs_dir: str = "./epochs",
+                   save=False,
+                   preload=False):
+        epochs_path = self.get_epochs_path(epochs_dir, self.run_id, self.subject)
+        if preload and epochs_path is not None and os.path.exists(epochs_path):
+            logger.info(f"reading epochs existing file: {epochs_path}")
+            self.epochs = read_epochs(epochs_path)
         else:
-            self.epochs = Epochs(self.raw, self.events, self.event_id, tmin, tmax, proj=True, picks=picks, preload=True)
+            if baseline:
+                self.epochs = Epochs(self.raw, self.events, self.event_id, tmin, tmax, proj=True, picks=self.picks, baseline=(tmin, tmin + 1.), preload=True)
+            else:
+                self.epochs = Epochs(self.raw, self.events, self.event_id, tmin, tmax, proj=True, picks=self.picks, preload=True)
+            if save and epochs_path is not None:
+                logger.info(f"Saving epochs to: {epochs_path}")
+                self.create_epochs_dir_if_not_exists()
+                self.epochs.save(epochs_path, overwrite=True)
         return self.epochs
 
 
@@ -147,17 +185,17 @@ def get_args():
                         help='Specify the path to MNE data')
     parser.add_argument('--subject', default=1,
                         help='Specify number of the subject you want to analyze')
-    parser.add_argument('--runs', default=3, choices=[1, 2, 3, 4, 5, 6],
+    parser.add_argument('--run', default=3, choices=[1, 2, 3, 4, 5, 6],
                         help=f"Specify this list of runs you want to analyze, cf this dictionnary: {runs_dict}")
     args = parser.parse_args()
-    args.runs = runs_dict[args.runs]
+    args.run = runs_dict[args.run]
     return args
 
 
 if __name__ == '__main__':
     args = get_args()
     show_plots = args.show_plots
-    parser = Parser(subject=args.subject, runs=args.runs, mne_path=args.data_path)
+    parser = Parser(subject=args.subject, run=args.run, mne_path=args.data_path)
     parser.load_data()
     montage = parser.select_montage('biosemi64')
     if show_plots:
@@ -176,4 +214,4 @@ if __name__ == '__main__':
     if show_plots:
         Visualizer.show_psd(parser.raw)
         Visualizer.show_psd(parser.raw, average=True)
-    print("Tadaaa")
+    logger.info("Tadaaa")
--- src/treatment_pipeline.py
+++ src/treatment_pipeline.py
@@ -1,12 +1,25 @@
 """Processing EEG data pipeline."""
 from tqdm import tqdm
+import pandas as pd
+import logging
+
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.model_selection import ShuffleSplit, cross_val_score, train_test_split
+
+from mne.decoding import CSP
+
 from utils import get_tpv_args, tasks
 from data_parsing import Parser
 
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("treatment")
+logger.setLevel(level=logging.INFO)
 
 class Treatment():
     def __init__(self, args):
-        self.subjects = [args.subject] if args.train or args.predict else range(0, 109)
+        # TODO replace subjects from 1 to 109:
+        self.subjects = [args.subject] if args.train or args.predict else range(1, 10)
         self.run_idxs = [args.run_idx] if args.train or args.predict else range(0, 4)
         self.runs = [x['runs'] for i, x in enumerate(tasks) if i in self.run_idxs]
         if args.predict:
@@ -14,32 +27,56 @@ class Treatment():
         else:
             self.train()
 
-    def get_epochs(self, run, subject, labels):
-        parser = Parser(subject=subject, runs=run, mne_path="./mne_data")
-        parser.load_data()
-        parser.select_montage('standard_1005')
-        parser.get_events()
-        parser.get_annotations(labels)
-        noisy_channels = ['AF7', 'AF3', 'AFz', 'AF4', 'AF8', 'Fp1', 'Fpz', 'Fp2', 'P7', 'P5', 'P3', 'P1', 'P2', 'P4', 'P6',
-                      'P8', 'PO7', 'PO3', 'POz', 'PO4', 'PO8', 'Iz']
-        parser.reduce_noise(60, noisy_channels)
-        parser.focus_and_clean(significant_frequencies=(7.0, 32.0))
-        parser.get_events()
-        print(parser.events.shape)
-        picks = parser.reduce_noise()
-        print(len(picks))
-        print(parser.raw)
-        return None
+    def build_pipe(self, lda_name: str = 'classic', csp_components: int = 4) -> Pipeline:
+        if (lda_name == 'shrimp'):
+            lda = LDA(solver='lsqr', shrinkage='auto')
+        elif lda_name == 'classic':
+            lda = LDA()
+        csp = CSP(n_components=csp_components)
+        pipeline = Pipeline([("CSP", csp), ("LDA", lda)])
+        return pipeline
 
+
+    def cross_val_pipeline(self, pipeline, epochs) -> float:
+        targets = epochs.events[:, -1]
+        epochs_data = epochs.copy().crop(tmin=1.0, tmax=4.0).get_data() # They do this cropping on MNE website. Why ?
+        scores = cross_val_score(pipeline, epochs_data, targets, cv=ShuffleSplit(10, test_size=0.2), n_jobs=None)
+        return scores.mean()
+
+
+    def train_pipeline(self, pipeline: Pipeline, epochs, test_size = 0.1) -> Pipeline:
+        targets = epochs.events[:, -1]
+        epochs_data = epochs.copy().crop(tmin=1.0, tmax=4.0).get_data() # They do this cropping on MNE website. Why ?
+
+        x_train, x_test, y_train, y_test = train_test_split(epochs_data, targets, test_size=test_size, random_state=0)
+
+        x_train, y_train = epochs_data, targets
+        pipeline.fit(X=x_train, y=y_train)
+        score = pipeline.score(X=x_test, y=y_test)
+        logger.info(f"trained_pipeline_score: {score:.2f}")
+        return pipeline
 
     def train(self):
-        for run_id in tqdm(self.run_idxs):
+        training_data = pd.DataFrame(columns=['subject', 'task_number', 'cross_val_score'])
+        for run_id in self.run_idxs:
             run = tasks[run_id]['runs']
             labels = tasks[run_id]['labels']
-            for subject in tqdm(self.subjects):
-                epochs = self.get_epochs(run, subject, labels)
-
-
+            logger.info(f"**************** RUN: {run}")
+            for subject in self.subjects:
+                parser = Parser(subject=subject, run=run, mne_path="./mne_data", run_id=run_id)
+                parser.motion_preprocessing(labels)
+                epochs = parser.get_epochs(epochs_dir="./epochs", save=True, preload=True)
+                pipe = self.build_pipe(lda_name='classic', csp_components=10)
+                cross_score = self.cross_val_pipeline(pipe, epochs)
+                logger.info(f"Run:{run},\t\tsubject:{subject},\t\tCross validation average:{cross_score:.2f}")
+                model_metrics = {
+                    'subject': subject,
+                    'task_number': run_id,
+                    'cross_val_score': cross_score,
+                }
+                metrics = pd.DataFrame(model_metrics, index=[0])
+                training_data = pd.concat([training_data, metrics])
+        training_data.to_csv("./results/training.csv")
 
     def predict(self):
         pass
